ba888073666ac1920273539190b6f57d9457215f
BillClintonAglomasa <aglomasa@yahoo.com>
2024-01-23 00:05:38 +0000

    Add safe_print_division function

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+def safe_print_division(a, b):
+    try:
+        divide = a/b
+    except (TypeError, ZeroDivisionError):
+        divide = None
+    finally:
+        print("Inside result: {}".format(divide))
+    return (divide)
